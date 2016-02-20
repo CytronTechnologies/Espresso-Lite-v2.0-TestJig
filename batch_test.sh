b94e4b3456140a5b41dc4bc890a40d60405f8ad2
bengchet <bengchet@cytron.com.my>
2016-02-20 16:35:00 +0000

    updated files

---
+++ batch_test.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+cd /
+cd home/pi
+
+#define variables here
+dir=$(mktemp -d)
+exit_value=0
+max_dev=6
+#
+
+let max_dev=max_dev-1
+
+sudo python msg_oled.py "Testing modules now..."
+
+for i in `seq 0 $max_dev` 
+do
+  { sudo python ./serialESP.py "/dev/ttyUSB$i"; echo "$?" > "$dir/$i" ;} &
+done
+
+wait
+
+dev1="Pass"
+dev2="Pass"
+dev3="Pass"
+dev4="Pass"
+dev5="Pass"
+dev6="Pass"
+val=1
+
+for file in "$dir"/*
+do
+ while IFS= read -r line
+ do
+  var="dev$val"
+  if [ "$line" -eq "255" ];then
+   declare "$var"="Fail"
+   exit_value=1
+  fi
+  #echo "${!var}"
+  #echo "$exit_value"
+ done <"$file"
+ ((val=val+1))
+done
+
+rm -r "$dir"
+
+#display result
+sudo python msg_oled.py "All test completed" "" " 1 : $dev1     4 : $dev4" " 2 : $dev2     5 : $dev5" " 3 : $dev3     6 : $dev6"
+
+cd /
+
+if [ "$exit_value" -eq 0 ];then
+  (exit 0)
+else
+  (exit 1)
+fi
+#exit 2
+#echo $?
