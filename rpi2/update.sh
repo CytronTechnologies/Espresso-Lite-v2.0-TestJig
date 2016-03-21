fa548a1774c8ec319c9da50a5869e3ad4ce4aa31
bengchet <bengchet@cytron.com.my>
2016-03-21 02:02:40 +0000

    updated update file

--- rpi2/update.sh
+++ rpi2/update.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-path1=GitHub/Espresso-Lite-v2.0-TestJig/rpi2/TestJig/_2000_ESPert_workshop.ino.bin
-path2=TestJig/_2000_ESPert_workshop.ino.bin
+path1=/home/pi/GitHub/Espresso-Lite-v2.0-TestJig/rpi2/TestJig/_2000_ESPert_workshop.ino.bin
+path2=/home/pi/TestJig/_2000_ESPert_workshop.ino.bin
 
-cd /home/pi/GitHub/*
-git pull
-cd
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+        cd /home/pi/GitHub/*
+	git pull
+	cd
+else
+        echo "Offline"
+        exit 0
+fi
 
 if diff "$path1" "$path2" ; then
    echo "Nothing changed"
 else
    echo "Something changed"
-   cp "$path1" ./TestJig
+   cp "$path1" /home/pi/TestJig
 fi
